30c665ff440b868e23ceb0906db13850f9c3ac6f
lab <lab@localhost.localdomain>
2020-12-02 08:06:42 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+const App: React.FC = () => {
+	const { data, loading } = useQuery(gql`
+		{
+			user
+		}
+	`);
+	if (loading) {
+		return <div>loading...</div>;
+	}
+	return <div className="App">{JSON.stringify(data)}</div>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+// import { } from "@apollo/client";
+// import { ApolloProvider } from "react-apollo";
+import App from "./App";
+// import {} from "apollo-boost";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+	uri: "http://localhost:4001/graphql",
+	cache: new InMemoryCache(),
+});
+
+render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const ApolloApp: ReactElement<()=>void> = () => {
+// 	<ApolloProvider client={client}>
+// 		<App />
+// 	</ApolloProvider>;
+// };
+// render(ApolloApp(), document.getElementById("root"));
